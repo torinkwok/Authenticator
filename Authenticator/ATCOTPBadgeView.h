c588573e54650f45dd6ed8a6c827eb813a816b94
Tong Kuo <contact@tong-kuo.me>
2016-02-07 13:53:14 +0800

    Ok go ahead

---
+++ Authenticator/ATCOTPBadgeView.h
@@ -0,0 +1,14 @@
+//
+//  ATCOTPBadgeView.h
+//  Authenticator
+//
+//  Created by Tong G. on 2/7/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// ATCOTPBadgeView class
+@interface ATCOTPBadgeView : NSControl
+
+@end // ATCOTPBadgeView class
---
+++ Authenticator/ATCOTPBadgeView.m
@@ -0,0 +1,21 @@
+//
+//  ATCOTPBadgeView.m
+//  Authenticator
+//
+//  Created by Tong G. on 2/7/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "ATCOTPBadgeView.h"
+
+// ATCOTPBadgeView class
+@implementation ATCOTPBadgeView
+
+- ( void ) drawRect: ( NSRect )_DirtyRect
+    {
+    [ super drawRect: _DirtyRect ];
+    
+    // Drawing code here.
+    }
+
+@end // ATCOTPBadgeView class
