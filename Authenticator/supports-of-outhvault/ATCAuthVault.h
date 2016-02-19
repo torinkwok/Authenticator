27b806b0abfc165b991038d042d8d0fde27f2d7c
Tong Kuo <contact@tong-kuo.me>
2016-02-19 15:28:14 +0800

    Reintroduced the fucking ATCAuthVault class

---
+++ Authenticator/supports-of-outhvault/ATCAuthVault.h
@@ -0,0 +1,12 @@
+//
+//  ATCAuthVault.h
+//  Authenticator
+//
+//  Created by Tong G. on 2/19/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+// ATCAuthVault class
+@interface ATCAuthVault : NSObject
+
+@end // ATCAuthVault class
---
+++ Authenticator/supports-of-outhvault/ATCAuthVault.m
@@ -0,0 +1,14 @@
+//
+//  ATCAuthVault.m
+//  Authenticator
+//
+//  Created by Tong G. on 2/19/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "ATCAuthVault.h"
+
+// ATCAuthVault class
+@implementation ATCAuthVault
+
+@end // ATCAuthVault class
