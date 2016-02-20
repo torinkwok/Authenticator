76dc3af2814499197f224621992fda4f4147b060
Tong Kuo <contact@tong-kuo.me>
2016-02-20 17:01:58 +0800

    Well well done

--- Authenticator/ATCAppDelegate.m
+++ Authenticator/ATCAppDelegate.m
@@ -41,55 +41,6 @@
     if ( !authVault )
         NSLog( @"%@", error );
     #endif
-
-//    NSString* keychainPath = [ NSHomeDirectory() stringByAppendingPathComponent: @"test.keychain" ];
-//    NSURL* keychainUrl = [ NSURL URLWithString: [ NSString stringWithFormat: @"file://%@", keychainPath ] ];
-//    WSCKeychain* newKeychain = [ [ WSCKeychainManager defaultManager ] createKeychainWithURL: keychainUrl passphrase: @"isgtforever" becomesDefault: NO error: nil ];
-//
-//    NSData* keychainData = nil;
-//    unsigned char buffer[ CC_SHA512_DIGEST_LENGTH ];
-//    NSData* bufferData = nil;
-//
-//    [ [ WSCKeychainManager defaultManager ] lockKeychain: newKeychain error: nil ];
-//
-//    //
-//    keychainData = [ NSData dataWithContentsOfURL: keychainUrl ];
-//    CCHmac( kCCHmacAlgMD5, @"fuckyou", @"fuckyou".length, keychainData.bytes, keychainData.length, &buffer );
-//    bufferData = [ NSData dataWithBytes: buffer length: CC_SHA512_DIGEST_LENGTH ];
-//    NSLog( @"%@", [ bufferData base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength ] );
-//    //
-//
-//    [ [ WSCKeychainManager defaultManager ] unlockKeychain: newKeychain withPassphrase: @"isgtforever" error: nil ];
-//    [ newKeychain addApplicationPassphraseWithServiceName: @"test" accountName: @"tongkuo" passphrase: @"tongkuo" error: nil ];
-//
-//    //
-//    keychainData = [ NSData dataWithContentsOfURL: keychainUrl ];
-//    CCHmac( kCCHmacAlgMD5, @"fuckyou", @"fuckyou".length, keychainData.bytes, keychainData.length, &buffer );
-//    bufferData = [ NSData dataWithBytes: buffer length: CC_SHA512_DIGEST_LENGTH ];
-//    NSLog( @"%@", [ bufferData base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength ] );
-//    //
-//
-//    [ newKeychain addApplicationPassphraseWithServiceName: @"test" accountName: @"tongkuo1" passphrase: @"tongkuo" error: nil ];
-//
-//    //
-//    keychainData = [ NSData dataWithContentsOfURL: keychainUrl ];
-//    CCHmac( kCCHmacAlgMD5, @"fuckyou", @"fuckyou".length, keychainData.bytes, keychainData.length, &buffer );
-//    bufferData = [ NSData dataWithBytes: buffer length: CC_SHA512_DIGEST_LENGTH ];
-//    NSLog( @"%@", [ bufferData base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength ] );
-//    //
-
-    // Encoding
-//    NSData* keychainData = [ NSData dataWithContentsOfFile: [ NSHomeDirectory() stringByAppendingPathComponent: @"lab.keychain" ] ];
-//
-//
-//    NSData* base64edData = [ keychainData base64EncodedDataWithOptions:
-//        NSDataBase64Encoding76CharacterLineLength | NSDataBase64EncodingEndLineWithCarriageReturn ];
-//
-//    NSString* base64edString = [ [ NSString alloc ] initWithData: base64edData encoding: NSUTF8StringEncoding ];
-//
-//    // Decoding
-//    NSData* decodedKeychainData = [ [ NSData alloc ] initWithBase64EncodedString: base64edString options: 0 ];
-//    [ decodedKeychainData writeToFile: [ NSHomeDirectory() stringByAppendingPathComponent: @"test.authvault" ] atomically: YES ];
     }
 
 @end // ATCAppDelegate class
