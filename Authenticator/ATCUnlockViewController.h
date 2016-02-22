aca5e254b194500cac4ceecb70232684ea53bfbd
Tong Kuo <contact@tong-kuo.me>
2016-02-22 11:32:24 +0800

    Introduced ATCUnlockViewController class

---
+++ Authenticator/ATCUnlockViewController.h
@@ -0,0 +1,14 @@
+//
+//  ATCUnlockViewController.h
+//  Authenticator
+//
+//  Created by Tong G. on 2/22/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// ATCUnlockViewController class
+@interface ATCUnlockViewController : NSViewController
+
+@end // ATCUnlockViewController class
---
+++ Authenticator/ATCUnlockViewController.m
@@ -0,0 +1,14 @@
+//
+//  ATCUnlockViewController.m
+//  Authenticator
+//
+//  Created by Tong G. on 2/22/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "ATCUnlockViewController.h"
+
+// ATCUnlockViewController class
+@implementation ATCUnlockViewController
+
+@end // ATCUnlockViewController class
