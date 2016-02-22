9445a78d4e715f8fb079b2b67949c7135e2fd001
Tong Kuo <contact@tong-kuo.me>
2016-02-22 11:33:26 +0800

    Introduced ATCUnlockWindowController class

---
+++ Authenticator/ATCUnlockWindowController.h
@@ -0,0 +1,14 @@
+//
+//  ATCUnlockWindowController.h
+//  Authenticator
+//
+//  Created by Tong G. on 2/22/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// ATCUnlockWindowController class
+@interface ATCUnlockWindowController : NSWindowController
+
+@end // ATCUnlockWindowController class
