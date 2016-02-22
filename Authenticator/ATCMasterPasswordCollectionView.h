e70930c23e78865242599b2a62920ed5178742dd
Tong Kuo <contact@tong-kuo.me>
2016-02-22 11:38:16 +0800

    Introduced master password collection related stuffs

---
+++ Authenticator/ATCMasterPasswordCollectionView.h
@@ -0,0 +1,14 @@
+//
+//  ATCMasterPasswordCollectionView.h
+//  Authenticator
+//
+//  Created by Tong G. on 2/22/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// ATCMasterPasswordCollectionView class
+@interface ATCMasterPasswordCollectionView : NSView
+
+@end // ATCMasterPasswordCollectionView class
---
+++ Authenticator/ATCMasterPasswordCollectionView.m
@@ -0,0 +1,14 @@
+//
+//  ATCMasterPasswordCollectionView.m
+//  Authenticator
+//
+//  Created by Tong G. on 2/22/16.
+//  Copyright © 2016 Tong Kuo. All rights reserved.
+//
+
+#import "ATCMasterPasswordCollectionView.h"
+
+// ATCMasterPasswordCollectionView class
+@implementation ATCMasterPasswordCollectionView
+
+@end // ATCMasterPasswordCollectionView class
